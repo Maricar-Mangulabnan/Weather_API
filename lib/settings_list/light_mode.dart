6c077bf3ce7ff923981c572334b61c88f5654945
RIANE-GAMBOA <rianegamboa4441@gmail.com>
2025-03-28 01:23:05 +0800

    Update light_mode.dart
    DONE - Update light_mode.dart

--- lib/settings_list/light_mode.dart
+++ lib/settings_list/light_mode.dart
@@ -1,1 +1,15 @@
-RIANE
+import 'package:flutter/cupertino.dart';
+
+class LightModePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Light Mode'),
+      ),
+      child: SafeArea(
+        child: Center(child: Text('Light Mode Page')),
+      ),
+    );
+  }
+}
