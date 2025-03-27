0d0237ea6e5d3d72991e72c5ff260f3a7268c094
RIANE-GAMBOA <rianegamboa4441@gmail.com>
2025-03-28 01:22:33 +0800

    Update location.dart
    DONE- Update location.dart

--- lib/settings_list/location.dart
+++ lib/settings_list/location.dart
@@ -1,1 +1,19 @@
-RIANE
+import 'package:flutter/cupertino.dart';
+
+class LocationPage extends StatelessWidget {
+  final String initialLocation;
+
+  const LocationPage({required this.initialLocation});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Location'),
+      ),
+      child: SafeArea(
+        child: Center(child: Text('Location Page: $initialLocation')),
+      ),
+    );
+  }
+}
